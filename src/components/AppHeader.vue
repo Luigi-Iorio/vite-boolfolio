caa3f477af0ee429b39a7f885bbff2837e381e63
Luigi-Iorio <luigi0iorio@gmail.com>
2024-02-18 12:50:25 +0100

    style: header personalizzation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,31 +4,48 @@ export default {};
 
 <template>
   <!-- header -->
-  <header class="bg-dark text-light">
-    <div
-      class="container d-flex align-items-center justify-content-between p-3"
-    >
-      <!-- titolo -->
-      <h1>Portfolio</h1>
-      <!-- /titolo -->
-
+  <header>
+    <nav class="container">
       <!-- link -->
-      <ul class="d-flex align-items-center gap-5 list-unstyled mb-0">
-        <li>
+      <ul
+        class="col-12 col-md-5 mx-auto py-2 d-flex align-items-center justify-content-center gap-4 list-unstyled mb-0"
+      >
+        <li class="rounded-5 py-1 px-3 py-md-3 px-md-5 fs-6 fs-md-4">
           <router-link :to="{ name: 'home' }" class="nav-link"
             >Home</router-link
           >
         </li>
-        <li>
+        <li class="rounded-5 py-1 px-2 py-md-3 px-md-5 fs-6 fs-md-4">
           <router-link :to="{ name: 'projects' }" class="nav-link"
             >Progetti</router-link
           >
         </li>
       </ul>
       <!-- /link -->
-    </div>
+    </nav>
   </header>
   <!-- /header -->
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+header {
+  height: 90px;
+  nav {
+    height: 100%;
+    ul {
+      height: 100%;
+
+      li {
+        font-weight: bold;
+        background-color: #3f3f3f;
+        border: 1px solid transparent;
+
+        &:hover {
+          background-color: #00f8b7;
+          color: #3f3f3f;
+        }
+      }
+    }
+  }
+}
+</style>
