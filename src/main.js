089a5f9231a0bbc520f5871104610e1ed866fabf
Luigi-Iorio <luigi0iorio@gmail.com>
2024-02-18 12:49:33 +0100

    feat: fontawesome

--- src/main.js
+++ src/main.js
@@ -5,4 +5,22 @@ import { router } from "./router";
 
 import App from "./App.vue";
 
-createApp(App).use(router).mount("#app");
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faLocationDot, faGithub, faLinkedinIn, faEnvelope);
+
+createApp(App)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
