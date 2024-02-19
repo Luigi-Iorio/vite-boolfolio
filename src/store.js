7226cfe3f11169c5a83c398edc45f6bae76717ba
Luigi-Iorio <luigi0iorio@gmail.com>
2024-02-19 17:47:01 +0100

    refactor: store refactor

--- src/store.js
+++ src/store.js
@@ -1,11 +1,16 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-  baseUrl: "http://127.0.0.1:8000",
-
-  apiParams: {
+  url: {
+    baseUrl: "http://127.0.0.1:8000",
     apiUrl: {
       projects: "/api/projects",
+    },
+  },
+
+  apiParams: {
+    key: {
+      searchWord: null,
     },
   },
 });
